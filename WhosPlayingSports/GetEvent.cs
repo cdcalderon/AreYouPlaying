7bd35be24f1d18981f917d0bc43a7f74932382ab
carlos calderon <cdcalderon@gmail.com>
2018-09-29 18:58:24 -0500

    Add Ta-Lib POC example

--- WhosPlayingSports/GetEvent.cs
+++ WhosPlayingSports/GetEvent.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using TicTacTec.TA.Library;
 
 namespace WhosPlayingSports
 {
@@ -18,6 +19,20 @@ namespace WhosPlayingSports
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req,
             ILogger log)
         {
+            int outBegIndex = 0;
+            int outNbElement = 0;
+           
+            var closePrices = new float[] {1,2,3,47,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 };
+            var outMovingAverages = new double[closePrices.Length];
+            var resultState = TicTacTec.TA.Library.Core.MovingAverage(
+                0,
+                closePrices.Length - 1,
+                closePrices, 10,
+                Core.MAType.Ema,
+                out outBegIndex,
+                out outNbElement,
+                outMovingAverages);
+
             var response = req.GetQueryParameterDictionary();
 
             if (response.TryGetValue("responseCode", out var responseCode))
@@ -27,7 +42,7 @@ namespace WhosPlayingSports
                     return new OkObjectResult(new
                     {
                         EventDateAndTime = DateTime.Now,
-                        Location = "Here",
+                        Location = "Here " + outMovingAverages[0],
                         MyResponse = "Yes",
                         Responses = new[] {
                         new { Name= "Carlos", Response = "Playing"},
